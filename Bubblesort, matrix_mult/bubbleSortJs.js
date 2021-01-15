403c9c6b791559ae8c7cd6de2630cc12f83926d5
FarishtaJayas <farishtajayas@gmail.com>
2021-01-15 20:22:16 +0600

    BubbleSort javascript file updated

---
+++ Bubblesort, matrix_mult/bubbleSortJs.js
@@ -0,0 +1,21 @@
+function myfunsc(inputArr) {
+   
+    let bubbleSort = (inputArr) => {
+     let len = inputArr.length;
+     for (let i = 0; i < len; i++) {
+         for (let j = 0; j < len; j++) {
+             if (inputArr[j] > inputArr[j + 1]) {
+                 let tmp = inputArr[j];
+                 inputArr[j] = inputArr[j + 1];
+                 inputArr[j + 1] = tmp;
+             }
+         }
+     }
+     return inputArr;
+ };
+    
+ }
+
+ var fs = require('fs');
+ var text = fs.readFileSync("./numbers.txt").bubbleSort;
+ var textByLine = text.split("\n")
