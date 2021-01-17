efcebc94ef7e73477f28b54f85428005fd152b4a
zahinahmed97 <zahin.ahmed@northsouth.edu>
2021-01-17 20:55:40 +0600

    Update matrix.py

--- Bubblesort, matrix_mult/matrix.py
+++ Bubblesort, matrix_mult/matrix.py
@@ -1,4 +1,3 @@
-
 with open('number2.txt') as f:
     l = [int(i) for i in f]
     part = 5
@@ -13,16 +12,24 @@ with open('number2.txt') as f:
             x.append(temp)
             start += part
             end += part
-    y = result = x   #copying x into 2 new arrays
+    y = x   #copying x into 2 new arrays
 
+result=[[0 for col in range(5)] for row in range(5)]
+
+
+for r in y:
+   print(r)
+   
+for r in x:
+   print(r)
+   
 # iterate through rows of X
-for i in range(len(X)):
+for i in range(len(x)):
    # iterate through columns of Y
-   for j in range(len(Y[0])):
+   for j in range(len(y[0])):
        # iterate through rows of Y
-       for k in range(len(Y)):
-           result[i][j] += X[i][k] * Y[k][j]
+       for k in range(len(y)):
+           result[i][j] += x[i][k] * y[k][j]
 
 for r in result:
    print(r)
-
